d1d354e61f063fa43e4e7ac1e3e56c2c874d646a
amal-davis <amald416@gmail.com>
2023-09-26 19:50:17 +0530

    Updated Files

--- softverses/settings.py
+++ softverses/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'softverses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': 'softverses',
         'USER':'root',
         'PASSWORD':'',
